5885c7cb1cb014742e88ed3155ef02a4dc7c7a7a
Mat-Bit <mateus.lbittencourt@gmail.com>
2018-05-25 19:14:50 -0300

    Criando os comandos

---
+++ comandos.sh
@@ -0,0 +1,24 @@
+# Comando para clonar o repósitório global pro repositório local
+git clone https://github.com/Mat-Bit/Minicurso-Github.git
+
+# Acessar a pasta que foi criada localmente
+cd Minicurso-Github/
+
+# mostra o status de alguma possivel modificação se já foi add
+git status
+
+# Exemplo de saida:
+# No ramo master
+# Your branch is up-to-date with 'origin/master'.
+# Changes not staged for commit:
+#   (utilize "git add <arquivo>..." para atualizar o que será submetido)
+#   (utilize "git checkout -- <arquivo>..." para descartar mudanças no diretório de trabalho)
+#
+# 	modified:   README.md
+#
+# Arquivos não monitorados:
+#   (utilize "git add <arquivo>..." para incluir o que será submetido)
+#
+# 	teste.py
+#
+# nenhuma modificação adicionada à submissão (utilize "git add" e/ou "git commit -a")
